c6c6d72a8d90377b728b231a51825d5038f0a679
kuronokurujp <kuronokurujp@gmail.com>
2023-07-01 19:24:47 +0900

    [modify] ブログをpythonanywhereで公開するための設定を追加

--- SampleDjango/blog/README.md
+++ SampleDjango/blog/README.md
@@ -10,10 +10,12 @@
 - [テンプレートフィルター](https://qiita.com/nachashin/items/d3f9cd637a9cecbda72c)
     - templateに埋め込んだ変数に対してタグを使ってフィルタリングをする
     - 表示文字列の数を制限したり、表示内容を加工する事ができる
+
 ## 利用パッケージ一覧
 - [django-markdown](https://pypi.org/project/django-markdownx/4.0.2/)
     - djangoのアプリにMarkdownが使えるようにするためのパッケージ
     - ブログアプリの記事をMarkdownで作る時に使える
+
 ## 勉強して知った用語一覧
 - [Slug(スラッグ)](https://school.dhw.co.jp/course/graphic/contents/w_slug.html)
     - URLの末尾につける事ができる任意の文字列
--- SampleDjango/blog/config/settings.py
+++ SampleDjango/blog/config/settings.py
@@ -9,23 +9,25 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pk3g-61u5ii%2p8qg_l&d2ja&$xy9v5++rc3ljov6ysfq@&o-#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'kuronokurujp/pythonanywhere.com']
 
 
 # Application definition
